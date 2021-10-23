a0fc6ea578a48f11d119da7ca62b314e430d214a
Dahan13 <corentincaugant2001@gmail.com>
2021-10-23 16:35:56 +0200

    Added new length vector to optimize solve function. Initialize function needs some corrections.

--- DPLL.py
+++ DPLL.py
@@ -32,27 +32,31 @@ def conjonctiveIsTrue(conjonctive, litterals):
             return False
     return True
 
+def generate_length_vector(conjonctive):
+    """This function will generate the length vector corresponding to the given conjonctive"""
+    return [len(conjonctive[i]) for i in range(len(conjonctive))]
+
 def simplify(conjonctive, litterals):
-    temp_conjonctive = copy.deepcopy(conjonctive)
-    for i in range(len(temp_conjonctive)):
-        clause = temp_conjonctive[i]
+    """This function simplify a conjonctive. For optimization purpose, the function works with a length vector instead of using the whole CNRF"""
+    length_vector = generate_length_vector(conjonctive) # Complexity of deepcopy : O(n²), complexity of length generation : O(n). Added to it the lessen complexity of not editing an array of array, it's quite simpler.
+    for i in range(len(conjonctive)):
+        clause = conjonctive[i]
         for element in clause:
+            if length_vector[i] == -1: # In case the clause is already satisfied, we can stop the for loop for this clause
+                break
+
             if abs(element) in litterals.keys() and litterals[abs(element)][0] != None:
                 bool_value = ((element > 0) == (litterals[abs(element)][0])) # We found the value of the litteral, depending of the bool assigned to it and if it's a negation or not
                 if bool_value: # if the litteral is True, we delete the clause he is in
-                    temp_conjonctive[i] = True # Instead of deleting the clause, we assign a special value to it because deletion will cause index issues in the for loop
-                else: # If the litteral is False, we remove it from the clause
-                    clause[clause.index(element)] = False
-        while False in clause:
-            clause.remove(False)
-        if len(clause) == 0: # if a clause is empty, all litterals inside are false so the conjonctive is false
+                    length_vector[i] = -1 # We indicate that the clause is True (we do not use True because the function may take a 1 for a True, see the last if of the function)
+                else: # If the litteral is False, we remove it from the clause, meaning there is one less term in the clause
+                    length_vector[i] -= 1
+        if length_vector[i] == 0: # if a clause is empty (it's length is 0), all litterals inside are false so the conjonctive is false
             return False
-    # Deletion of all True clauses once the loop is over
-    while True in temp_conjonctive:
-        temp_conjonctive.remove(True)
-    if len(temp_conjonctive) == 0: # if the conjonctive is empty, all clauses are true so the conjonctive is true
+    print(length_vector)
+    if length_vector.count(-1) == len(length_vector): # if all clauses are true (there is as mush satisfied clauses as there is clauses)
         return True
-    return temp_conjonctive
+    return conjonctive
 
 def forever_alone_litterals(conjonctive, litterals):
     """Assigns values to mono-litterals and checks for incompatibilites between mono-litterals"""
@@ -117,6 +121,7 @@ def initialize_solving(conjonctive, litterals):
     return litterals, simplified_conjonctive
 
 def to_be_modified(litterals):
+    """Give an array of the non definitive litterals"""
     modifiable = []
     modified = {}
     for i in litterals.keys():
@@ -126,6 +131,7 @@ def to_be_modified(litterals):
     return modifiable, modified
 
 def proceed(litterals, conjonctive, pile, modifiable_litterals, modified):
+    """Proceed to the next litteral withing the possibility tree"""
     # Case where no litterals were treated yet
     if pile == []:
         litteral = modifiable_litterals[0]
@@ -140,12 +146,14 @@ def proceed(litterals, conjonctive, pile, modifiable_litterals, modified):
     return litterals
 
 def fail(litterals, conjonctive, pile, modifiable_litterals, modified):
+    """Try to workaround a litteral if it's first value gave a negative result"""
     litteral = pile[-1]
     litterals[litteral] = (False, False)
     modified[litteral] += 1
     return litterals
 
 def back(litterals, conjonctive, pile, modifiable_litterals, modified):
+    """Go back in the possibility tree if current litteral won't yield any result"""
     # Case we went back to the beginning
     if len(pile) == 1 :
         exit("This conjonctive is not solvable")
@@ -168,7 +176,6 @@ def back(litterals, conjonctive, pile, modifiable_litterals, modified):
 
 def solve(litterals:dict, conjonctive):
     # We begin by simplifying all mono-litterals
-    original_conjonctive = copy.deepcopy(conjonctive)
     litterals, conjonctive = initialize_solving(conjonctive, litterals)
     modifiable_litterals, modified = to_be_modified(litterals)
     pile = []
@@ -179,8 +186,9 @@ def solve(litterals:dict, conjonctive):
     temp_litterals = proceed(temp_litterals, conjonctive,pile ,modifiable_litterals, modified)
     cal_12 = True
     while cal_12:
-        #print(f"A conjonctive : {conjonctive}\nlitteraux : {temp_litterals}\n\n")
+        print(f"conjonctive : {conjonctive}\nlitteraux : {temp_litterals}\n\n")
         conjonctive = simplify(conjonctive_save, temp_litterals)
+        print(conjonctive)
         if isinstance(conjonctive, bool):
             if conjonctive:
                 cal_12 = False
