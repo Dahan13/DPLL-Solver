36ca38e3972971811f46a8d415547666e2f16f7d
Dahan13 <corentincaugant2001@gmail.com>
2021-10-25 14:54:28 +0200

    Added random CNF generator

---
+++ CNF_generator.py
@@ -0,0 +1,27 @@
+import random as rd
+import sys
+
+def generate_conjonctive(n_clauses, n_literals):
+
+    # Generating litterals
+    literals = {}
+    for i in range(1, n_literals + 1):
+        literals[i] = (None, False)
+
+    # Generating conjonctive
+    conjonctive = []
+
+    for i in range(n_clauses):
+        clause = []
+        for j in range(1, n_literals + 1):
+            literal_value = rd.randint(0, 2)
+            # We take the following convention : 0 is for the literal is not in the clause, 1 it is, 2 it's negation is in the clause.
+            if literal_value == 1:
+                clause.append(j)
+            elif literal_value == 2:
+                clause.append(-j)
+
+        if len(clause) != 0:
+            conjonctive.append(clause)
+    return literals, conjonctive
+
