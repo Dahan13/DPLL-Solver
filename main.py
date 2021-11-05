90fed634993e3efdf707aa9868668c34ab8b95d0
Dahan1703 <corentincaugant2001@gmail.com>
2021-11-05 12:31:24 +0100

    Added queens problem generator

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def main():
         file_management.write_results(file_management.get_conjonctives(file_path), only_one_solution, naif, heuristic)
     else:
         conjonc_choice == False
-        rand_choice = int(input("Do you want a random conjonctive (0), a pigeon problem (1) or a draughts game (2) ? "))
+        rand_choice = int(input("Do you want a random conjonctive (0), a pigeon problem (1) or a N-queens problem (2) ? "))
         while rand_choice != 0 and rand_choice != 1 and rand_choice != 2:
             rand_choice = int(input("Sorry I didn't understand...\nRetry : "))
         if rand_choice == 0:
@@ -38,9 +38,8 @@ def main():
             n_pigeons = int(input("How many pigeons do you want ? "))
             file_management.write_results([CNF_generator.generate_pigeon(n_pigeons)], only_one_solution, naif, heuristic)
         elif rand_choice == 2:
-            # Insert mes couilles once it's done
-            print("Elisabeth will bury us all")
-            return
+            n_queens = int(input("Enter the size of the board : "))
+            file_management.write_results([CNF_generator.generate_queens(n_queens)], only_one_solution, naif, heuristic)
 
 
 main()
